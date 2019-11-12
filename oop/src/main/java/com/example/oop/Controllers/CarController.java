c7793cc47ef76fb3d6d422e8cb121dbe06c419b1
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-11-12 00:41:40 -0800

    including gcp config

--- oop/src/main/java/com/example/oop/Controllers/CarController.java
+++ oop/src/main/java/com/example/oop/Controllers/CarController.java
@@ -6,7 +6,6 @@ import com.example.oop.Repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
