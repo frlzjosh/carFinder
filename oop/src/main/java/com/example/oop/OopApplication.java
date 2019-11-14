074c172d7311436dbb3287a55986618d984c26a9
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-11-13 23:33:37 -0800

    adding reference to mysql db

--- oop/src/main/java/com/example/oop/OopApplication.java
+++ oop/src/main/java/com/example/oop/OopApplication.java
@@ -1,13 +1,16 @@
 package com.example.oop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.oop", "com.example.oop.Controllers"})
 @EnableJpaRepositories("com.example.oop.Repositories")
 @EntityScan("com.example.oop.Models")
+// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class OopApplication {
 
 	public static void main(String[] args) {
