499126a1d5d9b7cf66677b15fa9a63c2c00c38e0
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-11-30 15:57:22 -0800

    working on configuring gcp integration into my spring app

--- oop/src/main/java/com/example/oop/OopApplication.java
+++ oop/src/main/java/com/example/oop/OopApplication.java
@@ -1,20 +1,27 @@
 package com.example.oop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.oop", "com.example.oop.Controllers"})
 @EnableJpaRepositories("com.example.oop.Repositories")
 @EntityScan("com.example.oop.Models")
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class OopApplication {
+public class OopApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OopApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OopApplication.class, args);
 	}
 
 }
+
+
+
