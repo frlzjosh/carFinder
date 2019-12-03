2483c47ee02483fce6e4ba65ae71784887ade6c3
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-02 23:19:04 -0800

    i believe that my app might be ready to deploy onto appengine. will be trying inside of remote gcp console?

--- oop/src/main/java/com/example/oop/OopApplication.java
+++ oop/src/main/java/com/example/oop/OopApplication.java
@@ -1,5 +1,7 @@
 package com.example.oop;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,14 +14,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.example.oop.Models")
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class OopApplication extends SpringBootServletInitializer {
+	private static final Logger log = Logger.getLogger(OopApplication.class.getName());
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  		log.info("Your information log message.");
 		return application.sources(OopApplication.class);
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(OopApplication.class, args);
 	}
+
+	
 
 }
 
