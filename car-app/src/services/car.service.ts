ebf95e48438ff64c99d2bdba7d7b99eeae945298
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-12 23:40:56 -0800

    working on getting my headers to authorize my api calls in the front end

--- car-app/src/services/car.service.ts
+++ car-app/src/services/car.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { carMakes } from './../assets/data/car';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,10 @@ export class CarService {
 
   public carMake: string[] = []
 
-  constructor(public http: HttpClient) { }
+  constructor(
+    public http: HttpClient,
+    public oktaAuth: OktaAuthService
+  ) { }
 
 
   public getCarMakes(){
@@ -20,6 +24,20 @@ export class CarService {
   }
 
   public queryCars(Obj):Observable<any>{
+    let headers: HttpHeaders = new HttpHeaders();
+    const token = this.oktaAuth.getAccessToken().then(resp=>{
+      return resp
+    }) 
+
+    token.then(resp=>{
+      if(resp){
+        console.log('token: ', resp)
+        headers = headers.append('Authorization', resp)
+      }
+    })
+    headers = headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+    console.log('headers: ', headers)
     let params = new HttpParams()
       .set("userID", Obj.id)
       .set("make", Obj.make)
@@ -27,6 +45,6 @@ export class CarService {
       .set("year", Obj.year)
       .set("isSalvaged", Obj.isSalvaged)
 
-    return this.http.get('https://car-app-258808.appspot.com/createCar',{params:params} )
+    return this.http.get('https://car-app-258808.appspot.com/createCar',{params:params, headers: headers})
   }
 }
--- car-app/src/services/token.service.ts
+++ car-app/src/services/token.service.ts
@@ -1,34 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { OktaAuthService } from '@okta/okta-angular';
-import { resolve } from 'url';
-import { reject } from 'q';
+// import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { OktaAuthService } from '@okta/okta-angular';
+// import { resolve } from 'url';
+// import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenService implements HttpInterceptor{
+export class TokenService{
 
-  constructor(public oktaAuth: OktaAuthService) { }
+//   constructor(public oktaAuth: OktaAuthService) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // All HTTP requests are going to go through this method
-    console.log('INTERCEPTOR');
-    // We retrieve the token, if any
-    let token
-    token = this.oktaAuth.getAccessToken().then(resp=>{
-      return resp
-    }) 
+//   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+//     // All HTTP requests are going to go through this method
+//     console.log('INTERCEPTOR');
+//     // We retrieve the token, if any
+//     let token
+//     token = this.oktaAuth.getAccessToken().then(resp=>{
+//       return resp
+//     }) 
 
-    token.then(resp=>{
-      if(resp){
-        newHeaders = newHeaders.append('authtoken', resp);
-      }
-    })
-    let newHeaders = req.headers;
-    console.log(req.headers)
-    const authReq = req.clone({headers: newHeaders});
-    return next.handle(authReq);
-  }
+//     token.then(resp=>{
+//       if(resp){
+//         console.log(resp)
+//         newHeaders = newHeaders.append('authtoken', resp);
+//       }
+//     })
+//     let newHeaders = req.headers;
+//     console.log(req.headers)
+//     const authReq = req.clone({headers: newHeaders});
+//     return next.handle(authReq);
+//   }
 }
