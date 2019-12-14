606ee682d303d16ca177dd3c46eb2f74836a94ad
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-13 23:34:59 -0800

    changing all routes to my netlify domain

--- car-app/src/app/auth-routing.module.ts
+++ car-app/src/app/auth-routing.module.ts
@@ -10,7 +10,7 @@ import {environment} from './../environments/environment'
 
 export const oktaConfig = {
   issuer: 'https://dev-459112.okta.com/oauth2/default',
-  redirectUri: environment.clientServer+'/implicit/callback',
+  redirectUri: 'https://car-app.netlify.com/implicit/callback',
   clientId: '0oa25pgd7kGzHFvZF357',
   pkce: true,
   scope: ['openid', 'profile', 'email']
