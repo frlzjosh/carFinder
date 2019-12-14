e7dc36381b0aaa2389798594af9fb11cd7679bcc
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-14 01:00:46 -0800

    adding an Id notation back to to my users entity

--- oop/src/main/java/com/example/oop/Models/User.java
+++ oop/src/main/java/com/example/oop/Models/User.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -17,9 +15,7 @@ public class User implements Serializable{
      *
      */
     private static final long serialVersionUID = 1L;
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id", unique=true, nullable = false)
     String id;
 
