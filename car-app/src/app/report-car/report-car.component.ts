a8d2470f0e5211ee06101741aea305c893398e07
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-14 01:35:48 -0800

    cleaning up code and having my app create users on initialization instead of on a user click event

--- car-app/src/app/report-car/report-car.component.ts
+++ car-app/src/app/report-car/report-car.component.ts
@@ -43,7 +43,7 @@ export class ReportCarComponent implements OnInit {
   }
 
   async ngAfterViewInit(){
-    // this.beginUserProcess();
+    this.beginUserProcess();
   }
 
   async beginUserProcess(){
