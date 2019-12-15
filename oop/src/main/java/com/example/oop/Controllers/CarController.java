a304938d0ba6c326e432b7c433d55496a8b92506
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-14 21:45:30 -0800

    creating a new api that returns cars by id

--- oop/src/main/java/com/example/oop/Controllers/CarController.java
+++ oop/src/main/java/com/example/oop/Controllers/CarController.java
@@ -6,8 +6,10 @@ import com.example.oop.Models.Car;
 import com.example.oop.Repositories.CarRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +29,10 @@ public class CarController {
                 body.get("isSalvaged")
             )
         );
+    }
+
+    @GetMapping("/getCars")
+    public Car getCars(@RequestParam String userID){
+        return carRespository.findCarByUserId(userID);
     }
 }   
--- oop/src/main/java/com/example/oop/Repositories/CarRepository.java
+++ oop/src/main/java/com/example/oop/Repositories/CarRepository.java
@@ -4,12 +4,14 @@ package com.example.oop.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.example.oop.Models.Car;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, String> {
 
     // custom query to search to blog post by title or content
-    // List<Car> findCarByMakeAndId(String make);
+    List<Car> findCarByUserId(String userID);
     
 }
