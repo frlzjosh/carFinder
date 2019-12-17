f561876ea7bf2e7a303ce06b67e1bb3afa6aa192
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-12-17 01:39:43 -0800

    including @Transactional annotation again

--- oop/src/main/java/com/example/oop/Controllers/CarController.java
+++ oop/src/main/java/com/example/oop/Controllers/CarController.java
@@ -10,8 +10,8 @@ import com.example.oop.Repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Transactional
 public class CarController {
 
     @Autowired
